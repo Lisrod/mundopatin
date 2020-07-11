8488174ea8b87859c88d53906c8f495befbe9f2a
Lis <liselen@gmail.com>
2020-07-11 13:03:51 -0300

    Updates

---
+++ precache-manifest.a37957cc2b9b9577230deed1a005ba63.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e417c33bb1e9528674e72486e50b99",
+    "url": "/mundopatin/index.html"
+  },
+  {
+    "revision": "f8b9b0de9eca116824bd",
+    "url": "/mundopatin/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ca6b2e6c6c7105f49d22",
+    "url": "/mundopatin/static/css/main.b57da4f6.chunk.css"
+  },
+  {
+    "revision": "f8b9b0de9eca116824bd",
+    "url": "/mundopatin/static/js/2.912fe148.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mundopatin/static/js/2.912fe148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6b2e6c6c7105f49d22",
+    "url": "/mundopatin/static/js/main.42783ddf.chunk.js"
+  },
+  {
+    "revision": "5bd58716c0edf8c45e99",
+    "url": "/mundopatin/static/js/runtime-main.a187aecd.js"
+  },
+  {
+    "revision": "2495b422e1641bc6679c1a424547d39d",
+    "url": "/mundopatin/static/media/defaultBcg.2495b422.jpg"
+  },
+  {
+    "revision": "63c6969d67529b879b565d8176498a07",
+    "url": "/mundopatin/static/media/galeria-1.63c6969d.jpg"
+  },
+  {
+    "revision": "653e865f659b73047abb5760b7d61e40",
+    "url": "/mundopatin/static/media/galeria-2.653e865f.jpg"
+  },
+  {
+    "revision": "189e6536b8e004a83b15289be13c57a4",
+    "url": "/mundopatin/static/media/galeria-3.189e6536.jpg"
+  },
+  {
+    "revision": "f03b8eb56a4eeeb4f0635368b37795b7",
+    "url": "/mundopatin/static/media/galeria-4.f03b8eb5.jpeg"
+  }
+]);
